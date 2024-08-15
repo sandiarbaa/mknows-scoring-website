a2b066b790a65d538680cfce37a588d62d1924ab
sandiarbaa <sandiarbaa@gmail.com>
2024-08-15 11:45:10 +0700

    add: env configuration

--- app/utils/lib/axios.ts
+++ app/utils/lib/axios.ts
@@ -5,8 +5,8 @@ import axios from "axios";
 // });
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:3001",
-  // baseURL: "http://13.210.185.89/",
+  // baseURL: "http://localhost:3001",
+  baseURL: "http://13.210.185.89/",
   headers: {
     "Content-Type": "application/json",
   },
