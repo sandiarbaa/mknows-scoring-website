ffec67f1f7f9608cf01ffafd80e186fe34e8f15b
sandiarbaa <sandiarbaa@gmail.com>
2024-10-14 10:52:45 +0700

    add: welcome page

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,9 @@
-"use client";
-import { usePathname, useRouter } from "next/navigation";
-import { useEffect } from "react";
+import WelcomePage from "./(pages)/(welcomePage)/page";
 
 export default function Home() {
-  const pathname = usePathname();
-  const router = useRouter();
-  if (pathname === "/") {
-    router.push("/login");
-  }
-
   return (
-    <div>
-      <h1>M-Knows AI Scoring Website</h1>
-    </div>
+    <>
+      <WelcomePage />
+    </>
   );
 }
